c86ef9df5813d0acbb9b0c467762cc52a3bd3f9d
salaboy <salaboy@gmail.com>
2019-08-03 12:18:15 +0100

    Initial import

---
+++ src/main/java/com/salaboy/conferences/site/DemoApplication.java
@@ -0,0 +1,53 @@
+package com.salaboy.conferences.site;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
+
+@Controller
+class ConferenceSiteController {
+    @Value("${version:0}")
+    private String version;
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String conferenceAgenda = "http://conference-agenda";
+        String conferenceSponsors = "http://conference-sponsors";
+        String agendaString = "N/A";
+        String sponsorsString = "N/A";
+        try {
+            ResponseEntity<String> agenda = restTemplate.getForEntity(conferenceAgenda, String.class);
+            agendaString = agenda.getBody();
+
+        } catch (Exception e) {
+        }
+        try {
+            ResponseEntity<String> sponsors = restTemplate.getForEntity(conferenceSponsors, String.class);
+            sponsorsString = sponsors.getBody();
+        } catch (Exception e) {
+        }
+
+        model.addAttribute("version", version);
+        model.addAttribute("sponsors", agendaString);
+        model.addAttribute("agenda", sponsorsString);
+        return "index";
+    }
+
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head><title></title>
+    <link rel="stylesheet" href="tachyons.min.css">
+</head>
+<body class="ph3 pt0 pb4 mw7 center sans-serif">
+    <h1 class="f2 mb0"><span class="gold">Conference V</span>${version}</h1>
+    <p class="f5 mt1 mb4 lh-copy">SPONSORS BANNER: ${sponsors}</p>
+    <p class="f5 mt1 mb4 lh-copy">AGENDA BANNER: ${agenda}</p>
+</body>
+</html>
